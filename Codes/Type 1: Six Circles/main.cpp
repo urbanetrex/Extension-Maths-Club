9bbea23e19d4f83b5f56b863fe3ae05868b2604e
urbanetrex <urbanetrex@gmail.com>
2025-03-20 16:43:16 +1100

    Code

---
+++ Codes/Type 1: Six Circles/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a1(n), a2(n), a3(n), a4(n);
+    a1={n, n+1, n+2, n+4, n+3, n+5};
+    return 0;
+}
+
+/*
+
+*/
