45df128c983d9e5627566e5b70d0b2fa383fa973
urbanetrex <urbanetrex@gmail.com>
2025-03-20 16:57:30 +1100

    Code

--- Codes/Type 1: Six Circles/main.cpp
+++ Codes/Type 1: Six Circles/main.cpp
@@ -5,7 +5,7 @@ int main(){
     cin >> n;
     vector<int> a1(n), a2(n), a3(n), a4(n);
     a1={n, n+5, n+4, n+1, n+3, n+2};
-    a2={n+3, , , , , };
+    a2={n+3, n+2, n+1, n+4, n, n+5};
     a3={n, n+5, n+3, n+2, n+1, n+4};
     a4={n+1, n+4, n+2, n+3, n, n+5};
     return 0;
